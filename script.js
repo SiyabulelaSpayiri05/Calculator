452d84dea616e48c56214700f337d079dca34b67
Siyabulela Spayiri <douglassiyabulela98@gmail.com>
2025-06-07 11:32:21 +0200

    Event handlers

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+ //Referance display element
+ const display = document.getElementById('display');
+
+ //track if we have peformed
+ let justCalculate = false
+
+ function appendToDisplay(value) {
+    console.log('button pressed:', value);
+    alert('you pressed:' + value);
+ }
+
+ function clearDisplay() {
+    console.log('Clear button clicked');
+    alert('Clear button clicked');
+ }
+
+ function deleteLast() {
+    console.log('delete button clicked');
+    alert('delete button pressed');
+ }
+ function calculate() {
+    console.log('calculate button pressed');
+    alert('calculate or = button pressed')
+ }
+
+ document.addEventListener('DOMContentLoaded', function () {
+     console.log('calculator loaded successfully');
+      console.log('display elemnt',display);
+      if (display) {
+        console.log('Current display value:',display.value)
+      } else{
+        console.log('Display element not found')
+      }
+ })
+    
  
